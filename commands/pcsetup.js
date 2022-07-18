6f3c1f941a3dd3a2a1ce23dab7a47736f66d3a12
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-07-19 00:43:32 +0200

    i
    i

--- commands/pcsetup.js
+++ commands/pcsetup.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
         .setColor("PURPLE")
         .setDescription("We hebben een aantal PC Builds samen gesteld voor verschillende prijsklasses, klik hieronder op het bedrag dat jij wilt uitgeven aan een PC en krijg een lijst met producten die jij moet bestellen voor een goede build voor je budget!");
 
-        return message.channel.send(welkomEmbed);
+        return message.channel.send({ embeds: [welkomEmbed]});
 
 }
 
