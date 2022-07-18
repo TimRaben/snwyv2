73d8e987850334f4752a01b24696347aa2b244c8
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-07-19 00:40:32 +0200

    i
    i

--- commands/pcsetup.js
+++ commands/pcsetup.js
@@ -2,12 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    const embed = new discord.MessageEmbed()
+    var embed = new discord.MessageEmbed()
         .setTitle("PC Builds - Prijzen")
         .setColor("PURPLE")
         .setDescription("We hebben een aantal PC Builds samen gesteld voor verschillende prijsklasses, klik hieronder op het bedrag dat jij wilt uitgeven aan een PC en krijg een lijst met producten die jij moet bestellen voor een goede build voor je budget!");
 
-        message.channel.send(embed)
+        return message.channel.send(embed);
 
 }
 
