8695483fb1f899660d8b06a488d576d57a0c5b4d
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-07-18 23:31:40 +0200

    i
    i

--- commands/pc.js
+++ commands/pc.js
@@ -1,1 +1,21 @@
-test
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const row = new discord.MessageActionRow().addComponents(
+
+        new discord.MessageButton()
+        .setCustomId("€750,-")
+        .setLabel("€750,-")
+        .setStyle("LINK")
+
+
+    ); 
+
+    message.channel.send({content: "test bericht", components: [row] });
+
+}
+
+module.exports.help = {
+    name: "pc"
+}
