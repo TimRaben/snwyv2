44e4b5a216a61ee92a4698091cfebdc64045df54
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-08-14 18:29:55 +0200

    i
    i

--- index.js
+++ index.js
@@ -34,13 +34,13 @@ client.once("ready", () => {
 
 });
 
-let suggestionChannel = client.channels.cache.find(c => c.name == "test-development");
+const suggestionChannel = client.channels.cache.find(c => c.name == "test-development");
         if(message.channel == suggestionChannel) {
             if (message.author.bot) return;
             message.delete();
             let msg = message.content;
             let suggestionEmbed;
-            suggestionEmbed = new Discord.MessageEmbed()
+            suggestionEmbed = new discord.MessageEmbed()
                 .setTitle(`Suggestie van ${message.author.username}#${message.author.discriminator}`)
                 .setColor('RANDOM')
                 .setDescription(msg)
