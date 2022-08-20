3409a56524a66c917e24ba02fbda8048b1886344
TimRaben <87567520+TimRaben@users.noreply.github.com>
2022-08-20 16:14:23 +0200

    i
    i

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents, Collection, Interaction } = require("discord.js");
+const { Client, Intents, Collection } = require("discord.js");
 const botConfig = require("./botConfig.json");
 const fs = require("fs");
 
